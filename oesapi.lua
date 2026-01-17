545b6b955893408fa4781a4633c9bddebb40c403
dream6-e <15606442582@163.com>
2026-01-17 22:34:46 +0800

    Refactor API URL and load script content

--- oesapi.lua
+++ oesapi.lua
@@ -1,1 +1,3 @@
-https://api.jnkie.com/api/v1/luascripts/public/276cd4cb477f56ce87ca10e60369a72d00058774fcd155acb1f2f90692278e47/download
+local apiUrl = "https://api.jnkie.com/api/v1/luascripts/public/276cd4cb477f56ce87ca10e60369a72d00058774fcd155acb1f2f90692278e47/download"
+local scriptContent = game:HttpGet(apiUrl)
+loadstring(scriptContent)()
