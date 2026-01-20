04ced19e3ac4c63f76515aa0cc1c33a2650dcb4a
dream6-e <15606442582@163.com>
2026-01-20 20:12:01 +0800

    Add real-time English text replacement tool
    This script implements a real-time English text replacement tool, translating predefined English terms to Chinese in a game environment. It includes a user interface for controlling the text replacement process and managing the translation dictionary.

--- oesapi.lua
+++
@@ -1,3 +0,0 @@
-local apiUrl = "https://api.jnkie.com/api/v1/luascripts/public/276cd4cb477f56ce87ca10e60369a72d00058774fcd155acb1f2f90692278e47/download"
-local scriptContent = game:HttpGet(apiUrl)
-loadstring(scriptContent)()
